85aba2f976b075407a374662906374e054d12780
Hassan <hassan95it@gmail.com>
2018-04-30 13:23:13 +0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
    console.log('Connected to server.');
 
-   socket.emit('createMessage', {
-      from: 'Jen',
-      text: 'Hey! This is hussey'
-   });
+   // socket.emit('createMessage', {
+   //    from: 'Jen',
+   //    text: 'Hey! This is hussey'
+   // });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('New user connected.');
 
-   socket.emit('newMessage', {
-      from: 'Hussey',
-      text: 'Hey! What is going on',
-      createdAt: 123
-   });
+   // socket.emit('newMessage', {
+   //    from: 'Hussey',
+   //    text: 'Hey! What is going on',
+   //    createdAt: 123
+   // });
 
    socket.on('createMessage', (message) => {
       console.log('createMessage', JSON.stringify(message, undefined, 2));
+      io.emit('newMessage', {
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+      })
    });
 
    socket.on('disconnect', () => {
